0629109d62d6b3c03ac417d7c34066a3e95ed7c6
Apurva Deshpande <61773839+avd151@users.noreply.github.com>
2020-11-10 09:13:21 +0530

    Added new files

---
+++ Assignment-10/Q1-Find-nth-element.lisp
@@ -0,0 +1,17 @@
+
+;Find nth element in List
+
+; Function to find nth element of List
+
+
+(setq list '(1 2 3 4 5))
+
+(setq n (read))
+
+
+(defun nthlist (n list)
+  print((nth n list))
+)
+
+nthlist(n list)
+
