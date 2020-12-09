5cba779b0aa858a8d80402eed1421e1ba5d69e03
Apurva Deshpande <61773839+avd151@users.noreply.github.com>
2020-12-09 13:36:30 +0530

    Updated file

--- Assignment-10/Q1-Find-nth-element.lisp
+++ Assignment-10/Q1-Find-nth-element.lisp
@@ -15,3 +15,18 @@
 
 nthlist(n list)
 
+; Recursive program to print nth list element without suing predefined functions
+
+(defun help-func (n list)
+(cond 
+    ((= n 1)(car list))
+    (T (help-func (1-n) (cdr list)))))
+
+(defun no-nth (n list)
+(cond
+    ((or (< n 0) (>= n (length list))) 'invalid-index)
+    (T (help-func n list))))
+
+(print(no-nth 7 '(1 2 3 4 5 6 7 8 9 10)))
+
+
