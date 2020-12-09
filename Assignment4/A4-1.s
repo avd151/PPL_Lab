b2ab90671137bc28cca912cf3f8418cbd45c9a6b
Apurva Deshpande <61773839+avd151@users.noreply.github.com>
2020-12-09 10:02:55 +0530

    Added new files

---
+++ Assignment4/A4-1.s
@@ -0,0 +1,206 @@
+	.file	"A4-1.cpp"
+	.text
+	.section .rdata,"dr"
+_ZStL19piecewise_construct:
+	.space 1
+.lcomm _ZStL8__ioinit,1,1
+	.text
+	.globl	_Z11swapNumbersRiS_
+	.def	_Z11swapNumbersRiS_;	.scl	2;	.type	32;	.endef
+	.seh_proc	_Z11swapNumbersRiS_
+_Z11swapNumbersRiS_:
+.LFB1524:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$16, %rsp
+	.seh_stackalloc	16
+	.seh_endprologue
+	movq	%rcx, 16(%rbp)
+	movq	%rdx, 24(%rbp)
+	movq	16(%rbp), %rax
+	movl	(%rax), %eax
+	movl	%eax, -4(%rbp)
+	movq	24(%rbp), %rax
+	movl	(%rax), %edx
+	movq	16(%rbp), %rax
+	movl	%edx, (%rax)
+	movq	24(%rbp), %rax
+	movl	-4(%rbp), %edx
+	movl	%edx, (%rax)
+	nop
+	addq	$16, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+.LC0:
+	.ascii "Enter a Value for a : \0"
+.LC1:
+	.ascii "Enter a Value for b : \0"
+.LC2:
+	.ascii "Before swap, value of a \0"
+.LC3:
+	.ascii "\12\0"
+.LC4:
+	.ascii "Before swap, value of b \0"
+.LC5:
+	.ascii "After swap, value of a \0"
+.LC6:
+	.ascii "After swap, value of b \0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+.LFB1525:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$48, %rsp
+	.seh_stackalloc	48
+	.seh_endprologue
+	call	__main
+	leaq	.LC0(%rip), %rdx
+	movq	.refptr._ZSt4cout(%rip), %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	leaq	-4(%rbp), %rax
+	movq	%rax, %rdx
+	movq	.refptr._ZSt3cin(%rip), %rcx
+	call	_ZNSirsERi
+	leaq	.LC1(%rip), %rdx
+	movq	.refptr._ZSt4cout(%rip), %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	leaq	-8(%rbp), %rax
+	movq	%rax, %rdx
+	movq	.refptr._ZSt3cin(%rip), %rcx
+	call	_ZNSirsERi
+	leaq	.LC2(%rip), %rdx
+	movq	.refptr._ZSt4cout(%rip), %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movq	%rax, %rcx
+	movl	-4(%rbp), %eax
+	movl	%eax, %edx
+	call	_ZNSolsEi
+	leaq	.LC3(%rip), %rdx
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	leaq	.LC4(%rip), %rdx
+	movq	.refptr._ZSt4cout(%rip), %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movq	%rax, %rcx
+	movl	-8(%rbp), %eax
+	movl	%eax, %edx
+	call	_ZNSolsEi
+	leaq	.LC3(%rip), %rdx
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	leaq	-8(%rbp), %rdx
+	leaq	-4(%rbp), %rax
+	movq	%rax, %rcx
+	call	_Z11swapNumbersRiS_
+	leaq	.LC5(%rip), %rdx
+	movq	.refptr._ZSt4cout(%rip), %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movq	%rax, %rcx
+	movl	-4(%rbp), %eax
+	movl	%eax, %edx
+	call	_ZNSolsEi
+	leaq	.LC3(%rip), %rdx
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	leaq	.LC6(%rip), %rdx
+	movq	.refptr._ZSt4cout(%rip), %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movq	%rax, %rcx
+	movl	-8(%rbp), %eax
+	movl	%eax, %edx
+	call	_ZNSolsEi
+	leaq	.LC3(%rip), %rdx
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movl	$0, %eax
+	addq	$48, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	_Z41__static_initialization_and_destruction_0ii;	.scl	3;	.type	32;	.endef
+	.seh_proc	_Z41__static_initialization_and_destruction_0ii
+_Z41__static_initialization_and_destruction_0ii:
+.LFB1943:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	movl	%ecx, 16(%rbp)
+	movl	%edx, 24(%rbp)
+	cmpl	$1, 16(%rbp)
+	jne	.L6
+	cmpl	$65535, 24(%rbp)
+	jne	.L6
+	leaq	_ZStL8__ioinit(%rip), %rcx
+	call	_ZNSt8ios_base4InitC1Ev
+	movq	.refptr.__dso_handle(%rip), %r8
+	leaq	_ZStL8__ioinit(%rip), %rdx
+	movq	.refptr._ZNSt8ios_base4InitD1Ev(%rip), %rcx
+	call	__cxa_atexit
+.L6:
+	nop
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	_GLOBAL__sub_I__Z11swapNumbersRiS_;	.scl	3;	.type	32;	.endef
+	.seh_proc	_GLOBAL__sub_I__Z11swapNumbersRiS_
+_GLOBAL__sub_I__Z11swapNumbersRiS_:
+.LFB1944:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	movl	$65535, %edx
+	movl	$1, %ecx
+	call	_Z41__static_initialization_and_destruction_0ii
+	nop
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.section	.ctors,"w"
+	.align 8
+	.quad	_GLOBAL__sub_I__Z11swapNumbersRiS_
+	.ident	"GCC: (GNU) 9.3.0"
+	.def	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc;	.scl	2;	.type	32;	.endef
+	.def	_ZNSirsERi;	.scl	2;	.type	32;	.endef
+	.def	_ZNSolsEi;	.scl	2;	.type	32;	.endef
+	.def	_ZNSt8ios_base4InitC1Ev;	.scl	2;	.type	32;	.endef
+	.def	__cxa_atexit;	.scl	2;	.type	32;	.endef
+	.section	.rdata$.refptr._ZNSt8ios_base4InitD1Ev, "dr"
+	.globl	.refptr._ZNSt8ios_base4InitD1Ev
+	.linkonce	discard
+.refptr._ZNSt8ios_base4InitD1Ev:
+	.quad	_ZNSt8ios_base4InitD1Ev
+	.section	.rdata$.refptr.__dso_handle, "dr"
+	.globl	.refptr.__dso_handle
+	.linkonce	discard
+.refptr.__dso_handle:
+	.quad	__dso_handle
+	.section	.rdata$.refptr._ZSt3cin, "dr"
+	.globl	.refptr._ZSt3cin
+	.linkonce	discard
+.refptr._ZSt3cin:
+	.quad	_ZSt3cin
+	.section	.rdata$.refptr._ZSt4cout, "dr"
+	.globl	.refptr._ZSt4cout
+	.linkonce	discard
+.refptr._ZSt4cout:
+	.quad	_ZSt4cout
